eb5c67ce905f0a60f70f708e7dba7f4d9c613b86
Blair <tonnyblair09@gmail.com>
2024-03-21 18:04:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Function to handle scuberGreetingForFeet
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (feet > 2000 && feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+module.exports = {
+    scuberGreetingForFeet
+};
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+   
+    }
+  }
+
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip,
+};
